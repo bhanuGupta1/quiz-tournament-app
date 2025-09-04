bf70d80e026e2d43f0600b82c632fbd5a25c2500
bhanugupta1 <bhanugupta2001@gmail.com>
2025-09-04 23:35:55 +1200

    [PHASE1] Add UserRole enum for user authorization

---
+++ backend/src/main/java/com/quiztournament/quiz_backend/entity/UserRole.java
@@ -0,0 +1,11 @@
+package com.quiztournament.quiz_backend.entity;
+
+/**
+ * Enum defining user roles in the Quiz Tournament system
+ * ADMIN: Can create/manage tournaments and users
+ * PLAYER: Can participate in quiz tournaments
+ */
+public enum UserRole {
+    ADMIN,
+    PLAYER
+}
