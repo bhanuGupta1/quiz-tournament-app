c337ba4eebf5923715380f42308d537324ab685e
bhanugupta1 <bhanugupta2001@gmail.com>
2025-09-04 23:19:41 +1200

    [PHASE1] Add corrected pom.xml with JWT dependencies

---
+++ backend/src/test/java/com/quiztournament/quiz_backend/QuizBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiztournament.quiz_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
